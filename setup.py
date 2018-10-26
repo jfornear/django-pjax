342eabf8c322214f1066fcb71d7bdabaf99f4b9d
Jesse Fornear <jesse.fornear@gmail.com>
2018-10-26 03:50:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 import os
-import sys
-
-enc = sys.getdefaultencoding()
 
 try:
     from setuptools import setup
@@ -17,7 +14,7 @@ setup(
     version='1.4',
     description='Django-PJAX: The Django helper for jQuery-PJAX.',
     license='BSD',
-    long_description=open('README.rst', encoding=enc),
+    long_description=unicode('README.rst', 'utf-8'),
     url='https://github.com/eventials/django-pjax',
     author='Jacob Kaplan-Moss',
     author_email='jacob@jacobian.org',
